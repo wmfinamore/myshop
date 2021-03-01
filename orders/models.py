a39400852c680ff43138dd34ac7239ad311115b9
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-01 10:58:37 -0300

    Implemented checkout to place the orders from cart

--- orders/models.py
+++ orders/models.py
@@ -25,7 +25,7 @@ class Order(models.Model):
 
 class OrderITem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE, default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
