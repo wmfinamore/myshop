a157040e60dabd1afafdb71797774d76c5e97317
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-04 15:14:59 -0300

    Customized the Django admin to show details of a Order

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,8 @@ from .models import Order, OrderITem
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 
 class OrderItemInLine(admin.TabularInline):
@@ -35,11 +37,16 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInLine]
     actions = [export_to_csv]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import OrderITem
+from .models import OrderITem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
 
 
 def order_create(request):
@@ -34,3 +36,11 @@ def order_create(request):
                       'orders/order/create.html',
                       {'cart': cart,
                        'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
