f08a17b04ae12955e871b95a4d37b4827b369255
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-03-09 10:34:28 -0300

    Included pattern to insert language prefix in the URL's

--- myshop/urls.py
+++ myshop/urls.py
@@ -17,8 +17,25 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+
+# TODO remove this code
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('cart/', include('cart.urls', namespace='cart')),
+#     path('orders/', include('orders.urls', namespace='orders')),
+#     path('payment/', include('payment.urls', namespace='payment')),
+#     path('coupons/', include('coupons.urls', namespace='coupons')),
+#     path('rosetta/', include('rosetta.urls')),
+#     path('', include('shop.urls', namespace='shop')),
+# ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
@@ -26,7 +43,8 @@ urlpatterns = [
     path('coupons/', include('coupons.urls', namespace='coupons')),
     path('rosetta/', include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
-]
+)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
